3b2581b28354473a4bae21b6ecf726a760d50734
Hubert <36489757+ZerolBozi@users.noreply.github.com>
2021-12-02 10:50:07 +0800

    Update game.py

--- game.py
+++ game.py
@@ -4,34 +4,38 @@ import os
 #import pyttsx3
 
 def ChooseDifficulty():
-    global endrange
+    global srange
+    global erange
     global rndans
+
+    srange = 1
 
     difficulty = int(input("請選擇難易度，1.簡單 2.普通 3.困難（輸入數字）"))
     if difficulty == 1:
-        endrange = 50
+        erange = 50
     elif difficulty == 2:
-        endrange = 200
+        erange = 200
     elif difficulty == 3:
-        endrange = 500
+        erange = 500
     else:
         print("輸入的困難度錯誤，請重新輸入！！")
         ChooseDifficulty()
-    rndans = random.randrange(1,endrange)
+    rndans = random.randrange(srange,erange)
     print("遊戲將在1秒後開始...")
     time.sleep(1)
     os.system('cls')
     GameStart()
 
 def GameStart():
-    global gamepass
-    global wrongans
+    global srange
+    global erange
+    global rndans
 
     wrongans = 0
     gamepass = 0
 
     while gamepass == 0:
-        ans = input(f"請猜一個數字(範圍:1~{endrange})")
+        ans = input(f"請猜一個數字(範圍:{srange}~{erange})")
 
         if ans == "密技":
             print(f"答案為{rndans}")
@@ -46,14 +50,16 @@ def GameStart():
         
         if num == rndans:
             gamepass = 1
-        elif num > endrange or num < 1:
+        elif num > erange or num < srange:
             wrongans+=1
             print(f"{num}已超出範圍，請重新輸入")
         elif num != rndans:
             if num > rndans:
                 print("猜小一點")
+                erange = num
             else:
                 print("猜大一點")
+                srange = num
             wrongans+=1
 
     if gamepass:
