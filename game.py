6644184eee817c1a73bf7ca63f0637564fc6b41b
Hubert <36489757+ZerolBozi@users.noreply.github.com>
2021-12-02 14:19:29 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
+#coding=utf-8
 import random
 import time
 import os
 import pyttsx3
+
+rank1 = dict()
+rank2 = dict()
+rank3 = dict()
 
 def IsNumber(s):
     try:
@@ -16,7 +21,7 @@ def ChooseDifficulty():
     global srange
     global erange
     global rndans
-
+    global Ndifficulty
     srange = 1
 
     difficulty = input("請選擇難易度，1.簡單 2.普通 3.困難（輸入數字）")
@@ -43,9 +48,11 @@ def GameStart():
     global srange
     global erange
     global rndans
-
+    global Ndifficulty
     wrongans = 0
     gamepass = 0
+
+    st = time.time()
 
     while gamepass == 0:
         ans = input(f"請猜一個數字(範圍:{srange}~{erange})")
@@ -58,7 +65,10 @@ def GameStart():
             print("遊戲將於一秒後結束")
             time.sleep(1)
             break
-
+        
+        if not IsNumber(ans):
+            continue
+        
         num = int(ans)
         
         if num == rndans:
@@ -76,13 +86,22 @@ def GameStart():
             wrongans+=1
 
     if gamepass:
+        et = time.time()
         print("恭喜您猜對囉，猜數高手就是你")
         print(f"累計答錯題數：{wrongans}")
+        print("花費秒數：%.2f"%(round(et-st,2)))
+        record = GameRank(Ndifficulty,wrongans,round(et-st,2))
+        print("\n-----BestRecord-----")
+        print("難易度：%s"%(str(Ndifficulty).replace('1','簡單').replace('2','普通').replace('3','困難')))
+        print("累計答錯：%d"%record[0])
+        print("花費秒數：%.2f\n"%record[1])
+        
         engine = pyttsx3.init()
         engine.say(f"累計答錯題數：{wrongans}")
         engine.runAndWait()
+
         while True:
-            re = input("是否重新開始遊戲（y/n）")
+            re = input("是否重新開始遊戲？（y/n）")
             if re.lower() == 'y':
                 print("正在重新載入遊戲...")
                 time.sleep(1.5)
@@ -92,6 +111,30 @@ def GameStart():
             elif re.lower() == 'n':
                 input("輸入任意鍵結束遊戲")
                 break
+
+def GameRank(difficulty,nwrong,spend):
+    global rank1
+    global rank2
+    global rank3
+
+    r = ChooseRank(difficulty)
+    try:
+        ospend = r[nwrong]
+    except KeyError:
+        ospend = 0
+    if ospend == 0 or spend < ospend:
+        r[nwrong] = spend
+    tmplist = list(r.keys())
+    tmplist.sort()
+    return tmplist[0],r[tmplist[0]]
+
+def ChooseRank(difficulty):
+    if difficulty == 1:
+        return rank1
+    elif difficulty == 2:
+        return rank2
+    elif difficulty == 3:
+        return rank3
 
 if __name__ == '__main__':
     ChooseDifficulty()
